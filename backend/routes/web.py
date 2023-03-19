21d1a39962681ad9c8666ad2533163c800097f06
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2023-03-19 15:59:30 +0800

    feat(api): create api to accept/cancel signals

--- backend/routes/web.py
+++ backend/routes/web.py
@@ -1,7 +1,12 @@
+from datetime import datetime
+
 from database import Firestore
+from database.models import Location, Responder
 from flask import jsonify, request
 
 from routes import app
+
+SYSTEM = Responder(id="-1", telegram_id=-1, location=Location(-1, -1))
 
 
 @app.route("/distress", methods=["GET"])
@@ -10,3 +15,28 @@ async def get_distress_signals():
     distress_signals = await database.get_all_incomplete_distress()
 
     return jsonify([distress.to_dict() for distress in distress_signals])
+
+
+@app.route("/distress/accept/<id>", methods=["POST"])
+async def accept_distress_signals(id: int):
+    database = Firestore()
+    distress_signal = await database.get_distress(id)
+    distress_signal.acknowledged_at = str(datetime.now())
+    distress_signal.is_acknowledged = True
+    distress_signal.responder = SYSTEM
+
+    await database.update_distress(distress_signal)
+
+    return jsonify({"message": f"Distress signal {id} has been acknowledged"})
+
+
+@app.route("/distress/cancel/<id>", methods=["POST"])
+async def cancel_distress_signals(id: int):
+    database = Firestore()
+    distress_signal = await database.get_distress(id)
+    distress_signal.is_completed = True
+    distress_signal.responder = SYSTEM
+
+    await database.update_distress(distress_signal)
+
+    return jsonify({"message": f"Distress signal {id} has been cancelled"})
